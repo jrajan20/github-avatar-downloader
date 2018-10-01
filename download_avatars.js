8bc1025c107f8bae4fc390c3ed00c9e77a0bb53e
jrajan20 <jai_20j@hotmail.com>
2018-10-01 23:32:01 +0000

    avatar urls obtained

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,37 @@
 var request = require('request');
 var secret = require('./secrets');
-secret.GITHUB_TOKEN = aa6b90f566ebe654a5ce2d52188ade8c386dc648;
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb){
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
     }
 }
-  request(options, function(err, res, body) {
-    cb(err, body);
+var query = options;
+
+  request.get(query, function(err, res, body) {
+  	if (!err && res.statusCode == 200){
+  		var data = JSON.parse(body);
+  		console.log(data);
+    	cb(null, res, data);
+    
+  	} else {
+  		console.log(err)
+  	}
+   
+});
+}
+
+getRepoContributors("jquery", "jquery", function(err, result, body){
+	body.forEach(function(element){
+		console.log(element['avatar_url'])
+	});
 });
 
 
-getRepoContributors("jquery","jquery", function(err, result) {
-
-  // ...
-   console.log("Errors:", err);
-  console.log("Result:", result);
-
-});
-
-console.log(getRepoContributors())
+ 
