8f18dc4cf03062b1448780c33615b643372d7f49
jrajan20 <jai_20j@hotmail.com>
2018-10-02 00:40:48 +0000

    avatars downloaded and error checked

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var secret = require('./secrets');
+var fs = require('fs');
+var args = process.argv.slice(2)
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -14,24 +16,45 @@ function getRepoContributors(repoOwner, repoName, cb){
 }
 var query = options;
 
+
   request.get(query, function(err, res, body) {
-  	if (!err && res.statusCode == 200){
+  	if (!err && res.statusCode == 200 && repoOwner !== null && repoName !== null){
   		var data = JSON.parse(body);
   		console.log(data);
     	cb(null, res, data);
+    	//CB(data['avatar_url'], 'avatars/' + data['login'] + '.jpg')
+
     
   	} else {
-  		console.log(err)
+  		console.log(err);
+  		console.log("Error! Please enter Repo Owner and Repo Name");
   	}
+  
    
 });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result, body){
+getRepoContributors(args[0], args[1], function(err, result, body){
 	body.forEach(function(element){
 		console.log(element['avatar_url'])
+		downloadImageByURL(element['avatar_url'], 'avatars/' + element['login'] + '.jpg', element['login'])
+
 	});
 });
+
+function downloadImageByURL(url, filePath, name) {
+  request.get(url + filePath)               
+       .on('error', function (err) {                                   // Note 2
+         throw err; 
+       })
+       .on('response', function (response) {   
+                              
+         console.log('Response Status Code: ', response.statusCode, '\n', response.statusMessage, '\n', response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream('./'+ name + '.jpg' ));
+}
+
+
 
 
  
